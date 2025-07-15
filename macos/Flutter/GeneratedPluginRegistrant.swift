fafb2e4647f1435e7ceafd95fca754a90f2a77e6
mikolaj.dukiel <mikolaj.dukiel@systemeg.net>
2025-07-15 22:33:20 +0200

    soundpool

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
 import package_info_plus
 import path_provider_foundation
 import share_plus
+import soundpool_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
 }
